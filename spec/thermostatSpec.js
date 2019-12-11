aa74616a2d477455fbe239e62b4233a2faef992f
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2019-12-11 13:54:56 +0000

    min temp set

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,7 +7,29 @@ describe("Thermostat", function() {
   });
 
   it("starts at 20 degrees", function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.currentTemp()).toEqual(20);
 
+  });
+
+  it('has a minimum temperature set at 10', function(){
+    do { thermostat.down()} while (thermostat.currentTemp() > 10);
+  
+    expect(function() {
+      thermostat.down();
+    }).toThrowError('Temperature already at minimum');
+  });
+
+  describe('up', function (){
+    it("increase the temperature by 1", function(){
+    thermostat.up()
+    expect(thermostat.currentTemp()).toEqual(21);
+    });
+  });
+
+  describe('down', function() {
+    it('decrease the temperature by 1', function(){
+      thermostat.down()
+      expect(thermostat.currentTemp()).toEqual(19);
+    });
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,20 @@
+const MIN_TEMP =10
+
+
 function Thermostat() {
   this.temperature = 20;
+};
+
+Thermostat.prototype.currentTemp= function(){
+  return this.temperature
+
+};
+
+Thermostat.prototype.up = function(){
+  this.temperature += 1
+};
+
+Thermostat.prototype.down = function(){
+  if (this.temperature <= MIN_TEMP) throw new Error('Temperature already at minimum');
+  this.temperature --
 };
