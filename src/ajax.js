168759e623eeaf6e62301696c35e74cbb01da55a
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2019-12-13 14:44:18 +0000

    outside temperature API added

---
+++ src/ajax.js
@@ -0,0 +1,4 @@
+
+$.get('http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=357a681114dd79d8efab624598eb3c09&units=metric', function(data){
+    $('#current-temperature').text(data.main.temp);
+});
